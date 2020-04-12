8e45a4be3c2ef5a9606aad20b72b5260e83e161e
Ilham Surya Pratama <ilhamsuryapratama.github@gmail.com>
2020-04-13 01:53:09 +0700

    membuat jwt middleware

---
+++ app/Http/Middleware/JwtMiddleware.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use JWTAuth;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+use Tymon\JWTAuth\Http\Middleware\BaseMiddleware;
+
+class JwtMiddleware extends BaseMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        try {
+            $user = JWTAuth::parseToken()->authenticate();
+        } catch (Exception $e) {
+            if ($e instanceof TokenInvalidException) {
+                return response()->json(['status' => 'Token is Invalid']);
+            } elseif ($e instanceof TokenExpiredException) {
+                return response()->json(['status' => 'Token is Expired']);
+            } else {
+                return response()->json(['status' => 'Authorization Token not found']);
+            }
+        }
+
+        return $next($request);
+    }
+}
