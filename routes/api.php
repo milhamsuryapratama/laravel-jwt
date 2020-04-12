8dd717d8bf7848b1572acbff75ea50b04178a5dc
Ilham Surya Pratama <ilhamsuryapratama.github@gmail.com>
2020-04-13 02:14:42 +0700

    buat fixing routing

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,10 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('register', 'Auth\LoginController@register');
+Route::post('login', 'Auth\LoginController@login');
+Route::get('data', 'DataController@index');
+
+Route::get('dataall', 'DataController@dataAuth')->middleware('jwt.verify');
+Route::get('user', 'DataController@getAuthenticatedUser')->middleware('jwt.verify');
