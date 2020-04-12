bfe1b1be19e1a3ca2a3223fa45ab2106aadc79c6
Ilham Surya Pratama <ilhamsuryapratama.github@gmail.com>
2020-04-13 02:13:56 +0700

    pindah fungsi getAuthenticatedUser ke DataController

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+use JWTAuth;
 
 class DataController extends Controller
 {
@@ -15,5 +20,22 @@ class DataController extends Controller
     public function dataAuth() {
         $data = "Welcome " . Auth::user()->name;
         return response()->json($data, 200);
+    }
+
+    public function getAuthenticatedUser()
+    {
+        try {
+            if (!$user = JWTAuth::parseToken()->authenticate()) {
+                return response()->json(['user_not_found'], 404);
+            }
+        } catch (TokenExpiredException $e) {
+            return response()->json(['token_expired'], $e->getStatusCode());
+        } catch (TokenInvalidException $e) {
+            return response()->json(['token_invalid'], $e->getStatusCode());
+        } catch (JWTException $e) {
+            return response()->json(['token_absent'], $e->getStatusCode());
+        }
+
+        return response()->json(compact('user'));
     }
 }
